dc1e6283697195ddea91fe286b73ea6c4df9abbd
shehzada salman <shehzada.salman072@gmail.com>
2021-04-12 11:09:14 +0500

    updates

--- pages/MegaOffers.js
+++ pages/MegaOffers.js
@@ -34,28 +34,31 @@ const MegaOffers = ({ listing }) => {
                     <h2>
                       Riding the Supercar of Your Dreams is Just a Click Away!
                     </h2>
-                    The most famous and celebrated Mega Offers of RentMyRide are
-                    back! This time we are offering a 95% discount on the rental
-                    of a super or luxury car of your choice. Ohhhh Yes! You read
-                    that correctly! All you have to do is buy a subscription
-                    based on the number of days you wish to participate, and
-                    each day there will be a new surprise waiting for you in
-                    each category. You will know exactly when the offer will be
-                    active by the time tracker under the car. You can choose
-                    from the categories listed below. Each category has the most
-                    desirable cars, and you can have any car you, like but you
-                    have to be quick because the bookings are on a FIRST COME
-                    FIRST SERVE basis. If for some reason, you missed out on the
-                    deal, WORRY NOT! Because on the next day the offer will be
-                    active on another amazing Supercar. Here are the categories.
-                    <b>Premium</b>
-                    <b>Luxury</b>
-                    <b>Super Luxury</b>
-                    This is not all, you can have all your money back in your
-                    wallet if you don’t avail of any of the deals (T&amp;C
-                    Apply). So what are you waiting for? hurry up and subscribe
-                    for the most irresistible offer of the year and drive the
-                    waves of thrill every day.
+                    <p style={{ color: "#212121", fontSize: "16px" }}>
+                      The most famous and celebrated Mega Offers of RentMyRide
+                      are back! This time we are offering a 95% discount on the
+                      rental of a super or luxury car of your choice. Ohhhh Yes!
+                      You read that correctly! All you have to do is buy a
+                      subscription based on the number of days you wish to
+                      participate, and each day there will be a new surprise
+                      waiting for you in each category. You will know exactly
+                      when the offer will be active by the time tracker under
+                      the car. You can choose from the categories listed below.
+                      Each category has the most desirable cars, and you can
+                      have any car you, like but you have to be quick because
+                      the bookings are on a FIRST COME FIRST SERVE basis. If for
+                      some reason, you missed out on the deal, WORRY NOT!
+                      Because on the next day the offer will be active on
+                      another amazing Supercar. Here are the categories.
+                      <b>Premium</b>
+                      <b>Luxury</b>
+                      <b>Super Luxury</b>
+                      This is not all, you can have all your money back in your
+                      wallet if you don’t avail of any of the deals (T&amp;C
+                      Apply). So what are you waiting for? hurry up and
+                      subscribe for the most irresistible offer of the year and
+                      drive the waves of thrill every day.
+                    </p>
                   </div>
                 </div>
               </div>
